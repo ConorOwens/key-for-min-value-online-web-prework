5b24466c43070d92241cff36cfaf981e986f62f7
Conor Owens <conorj.owens@gmail.com>
2019-04-10 02:36:50 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,6 +7,7 @@ def key_for_min_value(name_hash)
   name_hash.map do |name, value|
     if i != nil && i > value
       i = value
+      answer = name
     elsif i == nil
       i = value
       answer = name
