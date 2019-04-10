1186382cb663ef18a46958d96b1dcb7cc3a7c14c
Conor Owens <conorj.owens@gmail.com>
2019-04-10 02:28:57 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,8 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  
+  i = nil 
   name_hash.map do |name, value|
-    
+    if i != nil
+      i = value
   end
 end
